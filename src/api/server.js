d9b7b6b51e9a54417429a78280d584c9c65f17a3
phvleite <phvleite@gmail.com>
2022-07-22 18:14:59 -0300

    guiado por Guilherme

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,7 @@
+const cors = require('cors');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   test: {
@@ -14,10 +16,10 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
 };
